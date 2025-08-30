f7de15a3ad3bdaf968c97a73b29ea96d22ace91a
Colton <Colton.Spruill@gmail.com>
2025-08-30 16:00:40 -0500

    Added some stuff for ads.
    It was a nightmare to build out again.

---
+++ STH/Assets/Scripts/LevelPlayAds.cs
@@ -0,0 +1,34 @@
+﻿using Unity.Services.LevelPlay;
+using UnityEngine;
+
+public class LevelPlayAds : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        LevelPlay.Init("2369c7645");
+    }
+
+    private void OnEnable()
+    {
+    }
+
+    private void PauseApplication(bool isPaused)
+    {
+        if (isPaused)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+
+
+    //Banner Callbacks
+
+    //Fullsize Callbacks
+
+    //Rewarded Ad Callbacks
+}
