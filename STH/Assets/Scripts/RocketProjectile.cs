08c4a34dd59aa155a0a51d00b581d516229c696a
admin <chris@postfmly.com>
2026-02-25 02:36:15 -0600

    fix: add pushback

--- STH/Assets/Scripts/RocketProjectile.cs
+++ STH/Assets/Scripts/RocketProjectile.cs
@@ -4,6 +4,7 @@ public class RocketProjectile : MonoBehaviour
 {
     [SerializeField] public float damage;
     [SerializeField] public float shootToHeal;
+    [SerializeField] public float bulletPushback;
     [SerializeField] public PlayerController playerController;
     [SerializeField] GameObject explosionPrefab;
 
@@ -15,11 +16,32 @@ public class RocketProjectile : MonoBehaviour
     {
         if (other.gameObject.GetComponent<RocketProjectile>() || other.gameObject.GetComponent<AIHelperBot>()) return;
 
+
         other.gameObject.GetComponent<Health>()?.TakeDamage(damage);
 
         playerController?.GetComponent<Health>()?.GiveHealth(shootToHeal);
 
-        other.gameObject.GetComponent<EnemyController>()?.PlayHurtAudio();
+        EnemyController enemyController = other.gameObject.GetComponent<EnemyController>();
+        enemyController?.PlayHurtAudio();
+
+        float newPushBackDur = 0f;
+        if (bulletPushback <= 0f)
+        {
+            newPushBackDur = 0f; // no pushback
+        }
+        else if (bulletPushback < 10f)
+        {
+            // starts at 0.15 when bulletPushback = 1
+            // increases by 0.1 each integer step
+            newPushBackDur = 0.05f + (0.10f * bulletPushback);
+        }
+        else
+        {
+            // clamp max at 10 → duration = 1.05f
+            newPushBackDur = 1.05f;
+        }
+        if (other.gameObject.GetComponent<Rigidbody>() && enemyController && bulletPushback > 0f)
+            enemyController.KnockBack(transform.forward, bulletPushback, newPushBackDur);
 
         Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius, damageableLayers);
         foreach (Collider collider in colliders)
